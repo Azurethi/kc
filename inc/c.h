48f9e26ae45f8f6d08bc7605cd2948373230e7be
Lewis McDermid <lmcdermid@firstderivatives.com>
2023-11-15 20:52:24 -0500

    add sizes

--- inc/c.h
+++ inc/c.h
@@ -3,14 +3,17 @@
 
 #include <stdint.h>
 
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef long long i64;  //~ for windows compat.
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+//~ fast ints?
+// long long instead of int64_t for windows compat.
+
+typedef int8_t i8;     // [−128, 127]
+typedef int16_t i16;   // [−32768, 32767]
+typedef int32_t i32;   // [−2147483648, 2147483647]
+typedef long long i64; // [−9223372036854775808, 9223372036854775807]
+typedef uint8_t u8;    // [0, 255]
+typedef uint16_t u16;  // [0, 65535]
+typedef uint32_t u32;  // [0, 4294967295]
+typedef uint64_t u64;  // [0, 18446744073709551615]
 
 typedef char ch;
 typedef const char cc;
