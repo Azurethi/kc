5102b8af0e7ec352c85a1eba54248201ffd21435
Lewis McDermid <lmcdermid@firstderivatives.com>
2023-11-23 20:28:49 -0500

    update kc.h

--- inc/kc.h
+++ inc/kc.h
@@ -4,7 +4,7 @@
 #define KXVER 3
 
 // k.h typedefs
-typedef char*S,C;
+typedef char *S, C;
 typedef unsigned char G;
 typedef short H;
 typedef int I;
@@ -14,40 +14,49 @@ typedef double F;
 typedef void V;
 typedef unsigned long long UJ;
 
-#ifdef __cplusplus
-extern"C"{
-#endif
-#if !defined(KXVER)
-// Asof 2023.03.30, default to 3. For kdb+2.x or standalone c-api before 2011-04-20 used KXVER=2
-#define KXVER 3
-#endif
-#if KXVER>=3
-typedef struct k0{signed char m,a,t;C u;I r;union{G g;H h;I i;J j;E e;F f;S s;struct k0*k;struct{J n;G G0[];};};}*K;
-typedef struct{G g[16];}U;
-#define kU(x) ((U*)kG(x))
-#define xU ((U*)xG)
-extern K ku(U),knt(J,K),ktn(I,J),kpn(S,J);
-extern I setm(I),ver();
-#define DO(n,x)	{J i=0,_i=(n);for(;i<_i;++i){x;}}
-#else
-typedef struct k0{I r;H t,u;union{G g;H h;I i;J j;E e;F f;S s;struct k0*k;struct{I n;G G0[];};};}*K;
-extern K ktn(I,I),kpn(S,I);
-#define DO(n,x)	{I i=0,_i=(n);for(;i<_i;++i){x;}}
-#endif
-#ifdef __cplusplus
-}
-#endif
+typedef struct k0
+{
+    signed char m;
+    signed char a;
+    signed char t;  // type
+    C u;
+    I r; // refcount
+    union
+    {
+        G g;
+        H h;
+        I i;
+        J j;
+        E e;
+        F f;
+        S s;
+        struct k0 *k;
+        struct
+        {
+            J n;
+            G G0[];
+        };
+    };
+} *K;
+typedef struct
+{
+    G g[16];
+} U;
+
+#define kU(x) ((U *)kG(x))
+extern K ku(U), knt(J, K), ktn(I, J), kpn(S, J);
+extern I setm(I), ver();
 
 // vector accessors, e.g. kF(x)[i] for float&datetime
-#define kG(x)	((x)->G0)
-#define kC(x)	kG(x)
-#define kH(x)	((H*)kG(x))
-#define kI(x)	((I*)kG(x))
-#define kJ(x)	((J*)kG(x))
-#define kE(x)	((E*)kG(x))
-#define kF(x)	((F*)kG(x))
-#define kS(x)	((S*)kG(x))
-#define kK(x)	((K*)kG(x))
+#define kG(x) ((x)->G0)
+#define kC(x) kG(x)
+#define kH(x) ((H *)kG(x))
+#define kI(x) ((I *)kG(x))
+#define kJ(x) ((J *)kG(x))
+#define kE(x) ((E *)kG(x))
+#define kF(x) ((F *)kG(x))
+#define kS(x) ((S *)kG(x))
+#define kK(x) ((K *)kG(x))
 
 //      type bytes qtype     ctype  accessor
 #define K0 0  // - gen list  -      kK
@@ -76,28 +85,22 @@ extern K ktn(I,I),kpn(S,I);
 #define XT 98 // - table     -      x->k is XD
 #define XD 99 // - dict      -      kK(x)[0] is keys. kK(x)[1] is values.
 
-#ifdef __cplusplus
-#include<cstdarg>
-extern"C"{
-extern V m9();
-#else
-#include<stdarg.h>
+#include <stdarg.h>
 extern V m9(V);
-#endif
+
 extern J gc(J j);
-extern I khpunc(S,I,S,I,I),khpun(const S,I,const S,I),khpu(const S,I,const S),khp(const S,I),okx(K),ymd(I,I,I),dj(I);extern V r0(K),sd0(I),sd0x(I d,I f),kclose(I);extern S sn(S,I),ss(S);
-extern K ee(K),ktj(I,J),ka(I),kb(I),kg(I),kh(I),ki(I),kj(J),ke(F),kf(F),kc(I),ks(S),kd(I),kz(F),kt(I),sd1(I,K(*)(I)),dl(V*f,J),m4(I),
- knk(I,...),kp(S),ja(K*,V*),js(K*,S),jk(K*,K),jv(K*k,K),k(I,const S,...),xT(K),xD(K,K),ktd(K),r1(K),krr(const S),orr(const S),dot(K,K),b9(I,K),d9(K),sslInfo(K x),vaknk(I,va_list),vak(I,const S,va_list),vi(K,UJ),m4(I);
-#ifdef __cplusplus 
-}
-#endif
+extern I khpunc(S, I, S, I, I), khpun(const S, I, const S, I), khpu(const S, I, const S), khp(const S, I), okx(K), ymd(I, I, I), dj(I);
+extern V r0(K), sd0(I), sd0x(I d, I f), kclose(I);
+extern S sn(S, I), ss(S);
+extern K ee(K), ktj(I, J), ka(I), kb(I), kg(I), kh(I), ki(I), kj(J), ke(F), kf(F), kc(I), ks(S), kd(I), kz(F), kt(I), sd1(I, K (*)(I)), dl(V *f, J), m4(I),
+    knk(I, ...), kp(S), ja(K *, V *), js(K *, S), jk(K *, K), jv(K *k, K), k(I, const S, ...), xT(K), xD(K, K), ktd(K), r1(K), krr(const S), orr(const S), dot(K, K), b9(I, K), d9(K), sslInfo(K x), vaknk(I, va_list), vak(I, const S, va_list), vi(K, UJ), m4(I);
 
 // nulls(n?) and infinities(w?)
 #define nh ((I)0xFFFF8000)
 #define wh ((I)0x7FFF)
 #define ni ((I)0x80000000)
 #define wi ((I)0x7FFFFFFF)
-#define nj ((J)0x8000000000000000LL) 
+#define nj ((J)0x8000000000000000LL)
 #define wj 0x7FFFFFFFFFFFFFFFLL
 
 #if defined(WIN32) || defined(_WIN32)
@@ -105,11 +108,11 @@ extern K ee(K),ktj(I,J),ka(I),kb(I),kg(I),kh(I),ki(I),kj(J),ke(F),kf(F),kc(I),ks
 #define wf (-log(0.0))
 #define finite _finite
 extern double log(double);
-#else  
-#define nf (0/0.0)
-#define wf (1/0.0)
+#else
+#define nf (0 / 0.0)
+#define wf (1 / 0.0)
 #define closesocket(x) close(x)
-#endif 
+#endif
 
 // Aliases of K for type annotation
 #define _K0 K // generic list
